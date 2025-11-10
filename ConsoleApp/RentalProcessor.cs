f16f3ab2fe00b87b4b5eb43ae0d1dc50c7f5123e
anastasiya <anastasiya.dubrouskaya@innowise.com>
2025-11-10 14:20:13 +0300

    feat: Implement XML data loading via LINQ to XML

---
+++ ConsoleApp/RentalProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+public class RentalProcessor
+{
+    public List<Rental> LoadRentals(string filePath)
+    {
+        if (!System.IO.File.Exists(filePath))
+        {
+            throw new System.IO.FileNotFoundException($"Входной файл не найден: {filePath}");
+        }
+
+        XDocument doc = XDocument.Load(filePath);
+
+        var rentals = doc.Descendants("Rental")
+            .Select(x => new Rental(int.Parse(x.Element("RentalID")?.Value ?? "0"))
+            {
+                CardNo = x.Element("CardNo")?.Value,
+                ClientName = x.Element("ClientName")?.Value,
+                StartDate = DateTime.Parse(x.Element("StartDate")?.Value),
+                EndDate = DateTime.Parse(x.Element("EndDate")?.Value),
+                AmountPaid = decimal.Parse(x.Element("Amount")?.Value),
+                Fine = decimal.Parse(x.Element("Fine")?.Value ?? "0.0")
+            })
+            .Where(Rental.IsValid)
+            .ToList();
+
+        return rentals;
+    }
+}
